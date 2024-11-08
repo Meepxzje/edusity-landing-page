739e9c6f77e8a5e733ea02184ec0b5d862858e31
Meepxzje <tmt.virut@gmail.com>
2024-11-08 13:56:38 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/edusity-landing-page/', // Thay "edusity-landing-page" bằng tên repository của bạn
   plugins: [react()],
+  base: '/edusity-landing-page/', // Set this to your repository name
 });
